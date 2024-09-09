bc54341479af334d2aee0c34715a67b3e4fc261e
Poe-chen <tranminh9@myvuw.ac.nz>
2024-09-09 13:18:24 +1200

    no spesaific chanfessh

--- music_starter.js
+++ music_starter.js
@@ -1,43 +1,71 @@
 
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
-function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
-  textFont('Verdana'); // please use CSS safe fonts
-  rectMode(CENTER)
-  textSize(24);
-  
-   let bar_spacing = height / 10;
-   let bar_height = width / 12;
-   let bar_pos_x = width / 2;
- 
-
-   // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-   // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
+function preload() {
+  // Load the image as bg
+  bgImage = loadImage('bg.png'); 
 }
+
+function setup() {
+  createCanvas(540, 960);
+  angleMode(DEGREES);
+}
+  //background(0);
+  //strokeWeight(4)}
+  
+// vocal, drum, bass, and other are volumes ranging from 0 to 100
+
+  //background(255);
+
+  let firstRun = true
+
+  let Mouth = [];
+  function draw_one_frame(words, vocal, drum, bass, other, counter){
+    if (firstRun){
+      rectMode(CENTER);
+      Mouth.push(loadImage('Mouth 1.png'));
+      Mouth.push(loadImage('Mouth 2.png'));
+      Mouth.push(loadImage('Mouth 3.png'));
+      
+      firstRun = false
+    }
+
+    var VocalFrame = int(map(vocal, 0, 100, 0, 2));
+    console.log(VocalFrame);
+    push();
+    scale(0.5)
+    Image(Mouth[VocalFrame], width/2, height/2)
+    pop();
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
